63b056eb1b49ca8fd9a5db6412301837ef3e0c70
Aye Myat Zabe Moe <ayemyatzabemoejob@gmail.com>
2025-07-17 13:58:06 +0630

    update

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -16,40 +16,40 @@ describe('Mytel Family Automation Testing', ()=>{
       .and('have.text',header_link.message.headerName)  
     })
 
-    // it('Search Valid Input Name Successfully', ()=>{
-    //   search.clickSearchIcon();
-    //   search.checkInputValue("Mytel Champion League 2024");
-    //   search.typeseachIcon();
-    //   search.verifyInputValue().should('be.visible');     
-    // })
+    it('Search Valid Input Name Successfully', ()=>{
+      search.clickSearchIcon();
+      search.checkInputValue("Mytel Champion League 2024");
+      search.typeseachIcon();
+      search.verifyInputValue().should('be.visible');     
+    })
 
-    // it('Search Invalid Input Name', ()=>{
-    //   search.clickSearchIcon();
-    //   search.checkInputValue("Mytel Information");
-    //   search.typeseachIcon();
-    //   search.elements.invalidsearchdisplay()
-    //   .should('be.visible')
-    //   .and('have.text',search.error_message.invalidsearchdisplay);
-    // })
+    it('Search Invalid Input Name', ()=>{
+      search.clickSearchIcon();
+      search.checkInputValue("Mytel Information");
+      search.typeseachIcon();
+      search.elements.invalidsearchdisplay()
+      .should('be.visible')
+      .and('have.text',search.error_message.invalidsearchdisplay);
+    })
 
-    // it('English Language Change Successfully', ()=>{
-    //     search.elements.headerTest().invoke('text')
-    //     .then((text)=>{
-    //       expect(text.trim()).to.eq(search.chooseLanguage.headerTest);
-    //     })
-    // })
+    it('English Language Change Successfully', ()=>{
+        search.elements.headerTest().invoke('text')
+        .then((text)=>{
+          expect(text.trim()).to.eq(search.chooseLanguage.headerTest);
+        })
+    })
 
-      // it('Burmese Language Change Successfully', ()=>{
-      //     search.clickLangugeIcon()
-      //     search.elements.headerTest1().invoke('text')
-      //     .then((text)=>{
-      //       expect(text.trim()).to.eq(search.chooseLanguage.headerTest1);
-      //     })
-      // })   
+      it('Burmese Language Change Successfully', ()=>{
+          search.clickLangugeIcon()
+          search.elements.headerTest1().invoke('text')
+          .then((text)=>{
+            expect(text.trim()).to.eq(search.chooseLanguage.headerTest1);
+          })
+      })   
 
-      //  it('Burmese Languagae Change Successfully', ()=>{
-      //       search.clickLangugeIcon()
-      //       search.elements.headerTest1()
-      //       .should('be.visible')
-      //  })
+       it('Burmese Languagae Change Successfully', ()=>{
+            search.clickLangugeIcon()
+            search.elements.headerTest1()
+            .should('be.visible')
+       })
 })
